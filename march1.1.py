ea2a6e0eca2ed4bc0f02e8aaf5b586281b75e7ea
AADIVENGAT <88031195+AADIVENGAT@users.noreply.github.com>
2022-03-01 20:08:23 +0530

    Add files via upload

---
+++ march1.1.py
@@ -0,0 +1,14 @@
+msg = "i Love Programming"
+print(msg[2])
+print(msg.upper())
+print(msg.lower())
+print(msg.capitalize())
+
+m = "iloveprogramming"
+print(m.isupper())
+print(m.islower())
+print(m.isalnum())  #space removal so false
+
+ms = "      I Love Programming     "
+print(ms.lstrip())  #left sapce
+print(ms.rstrip())  #right space
